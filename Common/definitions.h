bc793d2436d0e92e96d6c9e6ed838ddffc594d55
Yosame <2503542239@qq.com>
2024-04-25 21:54:02 +0800

    Add Shanghai

--- Common/definitions.h
+++ Common/definitions.h
@@ -6,12 +6,12 @@
 #define TRACEFILE "../test_sampled.txt"
 #define ROADVECTOR "../Intermediate/road_vectors.txt"
 #define GRIDSIZE 0.001 // 0.001 latitude = 111.195m
-#define PATH_NUM 65536
-#define TIMEZONE 0
+#define PATH_NUM 200000
+#define TIMEZONE 8
 #define SIGZ 4.07 // Related to the confidence of the GPS location point. The more reliable the location point is, the smaller σ_z should be.
 #define BETA 2 // Affects the confidence calculated by the size of the difference between Great Circle Distance and Route Distance
 #define EPS 1e-5
-#define RECOVER_INTERVAL 15
+#define RECOVER_INTERVAL 10 // The interval of the recovery of the GPS location point
 #define vec_len 13
 
 // Macros for [FuncEst.h/cpp] to fit parameters
